abc5324ed4c50018e4c26496d636f6307fe65175
Viktor Nymark <vnymark@gmail.com>
2016-11-01 00:53:28 +0100

    Working MVC.

---
+++ MVC_User/Controllers/DefaultController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_User.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Books()
+        {
+            ServiceReference.Service1Client Client = new ServiceReference.Service1Client();
+            List<ServiceReference.Book> Books = Client.GetBooks().ToList();
+            return View(Books);
+        }
+    }
+}
--- WindowsFormsAdmin/DeleteBook.Designer.cs
+++ WindowsFormsAdmin/DeleteBook.Designer.cs
@@ -30,7 +30,6 @@
         {
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
@@ -45,23 +44,13 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(350, 456);
+            this.button1.Location = new System.Drawing.Point(170, 383);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(266, 63);
+            this.button1.Size = new System.Drawing.Size(282, 63);
             this.button1.TabIndex = 5;
             this.button1.Text = "Delete Book";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(40, 456);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(266, 63);
-            this.button2.TabIndex = 6;
-            this.button2.Text = "Cancel";
-            this.button2.UseVisualStyleBackColor = true;
             // 
             // label1
             // 
@@ -79,7 +68,6 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(656, 544);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Name = "DeleteBook";
@@ -93,7 +81,6 @@
 
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label1;
     }
 }
