425d946ca096ea2065d674442ecf891387402277
Viktor Nymark <vnymark@gmail.com>
2016-11-01 02:38:41 +0100

    Updated database with some proper test/demo data.

--- WcfService/Model.Designer.cs
+++ WcfService/Model.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Wezno\documents\visual studio 2015\Projects\AVP400\WcfService\Model.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Wezno\Documents\Visual Studio 2015\Projects\AVP400\WcfService\Model.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
