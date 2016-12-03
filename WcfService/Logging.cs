0272c7277d89cb8cd792e6b9e33ddd9357e581b1
Viktor Nymark <vnymark@gmail.com>
2016-12-03 17:52:53 +0100

    Added comments explaining the logging function.

--- WcfService/Logging.cs
+++ WcfService/Logging.cs
@@ -10,7 +10,13 @@ namespace WcfService
     {
         public static void log(string message)
         {
-            
+            //The logging writes a line containing the string sent to the function(the message created through altering data).
+            //It also adds the current date and time from the local machine it runs from, to the same line of text.
+            //The parameter "true" is added so that new logs are added to the file, instead of overwriting it.
+            //I added put it into a try-catch just in case something goes wrong. 
+            //But I really don't want to log a logging error, in case that would case an infinite loop.
+            //You could how ever add a function to send an email to an administrator for if/when there is a logging error. 
+            //Inspiration from: http://stackoverflow.com/questions/5057567/how-to-do-logging-in-c
             try
             {
                 System.IO.StreamWriter file = new System.IO.StreamWriter(@"C:\Users\Wezno\Documents\Skola\AVP400\log.txt", true);
@@ -20,11 +26,7 @@ namespace WcfService
             catch (Exception)
             {
 
-                
             }
-            
-            
-
         }
     }
     
